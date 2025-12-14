ff00198cbb53ea68e8e14dcf1b7453fbd7d31847
navid80 <naviddavoudi80@gmail.com>
2025-12-14 18:42:56 +0330

    Implementation of the Second Exercise

---
+++ HomeWork_2/HomeWork_2/Exercise_1/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_2.Exercise_1
+{
+    public class Student
+    {
+        private string name;
+        private int age;
+
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name { get { return name; } }
+        public int Age { get { return age; } }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello , my name is {name} and I am {age} years old");
+        }
+    }
+}
---
+++ HomeWork_2/HomeWork_2/Exercise_3/Statistics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_2.Exercise_3
+{
+    public class Statistics
+    {
+        public void AnalyzeNumbers(int firstNumber, ref int secondNumber, out int ThirdNumber)
+        {
+            ThirdNumber = firstNumber + secondNumber;
+            secondNumber = secondNumber * secondNumber;
+            Console.WriteLine($"First Number : {firstNumber}");
+        }
+    }
+}
